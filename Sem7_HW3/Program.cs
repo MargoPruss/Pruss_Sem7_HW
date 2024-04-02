a2be03c975ff7c561b3d256ca66d172d94f28043
Margo <r.pruss@yandex.ru>
2024-04-02 22:43:18 +0300

    Sem7_HW3

---
+++ Sem7_HW3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+// Пример
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+
+void PrintReverseArray (int[] array, int i)
+{
+    if(i < 0) return;
+    Console.Write($"{array[i]} ");
+    PrintReverseArray(array, i - 1);
+}
+
+int[] array = {1, 2, 5, 0, 10, 34};
+
+PrintReverseArray(array, array.Length - 1);
