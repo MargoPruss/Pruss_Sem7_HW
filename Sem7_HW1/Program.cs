2d4714178941d3cb75f74261b0b192ad30a0dbd1
Margo <r.pruss@yandex.ru>
2024-04-02 21:52:08 +0300

    Sem7_HW1

--- Sem7_HW1/Program.cs
+++ Sem7_HW1/Program.cs
@@ -5,3 +5,27 @@
 // Пример
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void PrintNaturalNumbers(int M, int N)
+{
+   if(M > N)
+    {
+    Console.Write($"{M} ");
+    PrintNaturalNumbers(M - 1, N);
+    }
+    if(M < N)
+    {
+    PrintNaturalNumbers(M, N - 1);
+    Console.Write($"{N} ");
+    }
+}
+
+Console.WriteLine("Введите первое натуральное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M < N) Console.Write(M + " ");
+PrintNaturalNumbers(M, N);
+if (M > N) Console.Write(N);
